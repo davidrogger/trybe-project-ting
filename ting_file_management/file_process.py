2876e275e316fbe983ba7050a8115a9b7955fde6
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-01 15:17:17 -0300

    feat: implemented method process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,31 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
+def is_new_file(file_name: str, instance: Queue):
+    instance_length = len(instance)
+    for index in range(instance_length):
+        element = instance.search(index)
+        if element["nome_do_arquivo"] == file_name:
+            return False
+
+    return True
+
+
+def process(path_file: str, instance: Queue):
+    if is_new_file(path_file, instance):
+        file_lines = txt_importer(path_file)
+
+        file_report = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(file_lines),
+            "linhas_do_arquivo": file_lines,
+        }
+
+        instance.enqueue(file_report)
+
+        return sys.stdout.write(str(file_report))
 
 
 def remove(instance):
