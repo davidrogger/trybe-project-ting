946c7e844806802748b8440e611b0be5e05d487a
Davíd Roggér <davidrogger.a@gmail.com>
2022-12-01 15:06:40 -0300

    feat: txt importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import sys
+
+
+def txt_importer(path_file: str):
+    valid_extension = "txt"
+    if not path_file.endswith(valid_extension):
+        return sys.stderr.write("Formato inválido\n")
+    else:
+        try:
+            with open(path_file, "r") as file:
+                file_content = file.read()
+                lines = file_content.split("\n")
+        except FileNotFoundError:
+            return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        else:
+            return lines
